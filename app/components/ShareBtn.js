3d514d2a4d58289066992d90d28c09820fcf7fdd
joseForth <jose@forthwithlife.co.uk>
2022-12-02 11:50:49 +0000

    Add files

--- app/components/ShareBtn.js
+++ app/components/ShareBtn.js
@@ -2,15 +2,17 @@
 
 import React, { memo } from 'react'
 import styles from './ShareBtn.module.css'
+import forthLogo from './forthGradientIcon.png'
 
 export default memo(function ShareBtn(props) {
   const {children: text, title, url} = props
-  
+
   const handleShare = async () => {
     const shareData = {
       title,
       text,
       url,
+      file: [forthLogo]
     }
 
     try {
@@ -19,14 +21,15 @@ export default memo(function ShareBtn(props) {
     } catch (err) {
       console.error(`Error: ${err}`)
     }
+
   }
 
-  // console.log("canShare", globalThis.navigator?.canShare())
   return (
-    // globalThis.navigator?.canShare()
-    <button className={`${styles.shareBtn} text-center text-center`} onClick={handleShare}>
-      <i className="far fa-share-square fa-fw fa-1x me-1"></i>
-      Share code
-    </button>
+    <>
+      <button className={`${styles.shareBtn} text-center text-center`} onClick={handleShare}>
+        <i className="far fa-share-square fa-fw fa-1x me-1"></i>
+        Share code
+      </button>
+    </>
   );
 })
