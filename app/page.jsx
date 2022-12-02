1cb462a6591ee6b64f060f84c617cff351614d38
joseForth <jose@forthwithlife.co.uk>
2022-12-02 11:25:22 +0000

    Update page.jsx
    Delete default staff

--- app/page.jsx
+++ app/page.jsx
@@ -16,15 +16,6 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
         <div className={styles.grid}>
           <ShareBtn>
             {`Try out Forth and get £10 off your first test on me when you use my referral code: XXXX-XXXX-XXXX during purchase. To view what tests might be right for you, visit ${getWebsiteUrl(1)}`}
