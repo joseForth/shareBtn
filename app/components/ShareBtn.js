44fa9dfc65b560c0fbe9b3eb0e6333b7b70b0973
joseForth <jose@forthwithlife.co.uk>
2022-12-02 12:24:20 +0000

    Implement react-web-share

--- app/components/ShareBtn.js
+++ app/components/ShareBtn.js
@@ -4,6 +4,8 @@ import React, { memo } from 'react'
 import styles from './ShareBtn.module.css'
 import forthLogo from './forthGradientIcon.png'
 
+import { RWebShare } from "react-web-share";
+
 export default memo(function ShareBtn(props) {
   const {children: text, title, url} = props
 
@@ -30,6 +32,20 @@ export default memo(function ShareBtn(props) {
         <i className="far fa-share-square fa-fw fa-1x me-1"></i>
         Share code
       </button>
+
+      <RWebShare
+        data={{
+          text: "Like humans, flamingos make friends for life",
+          url: "https://on.natgeo.com/2zHaNup",
+          title: "Share this article on Flamingos"
+        }}
+        onClick={() => console.info("share successful!")}
+      >
+        <button className={`${styles.shareBtn} text-center text-center`}>
+          <i className="far fa-share-square fa-fw fa-1x me-1"></i>
+          Share option 2
+        </button>
+      </RWebShare>
     </>
   );
 })
